6de708dc00b23d43f21b1eaa1472bfbfbcdfa5d7
Bayli Boston <bpbh8v@mst.edu>
2018-11-27 11:54:58 -0600

    tested some things

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const bodyParser = require('body-parser')
 const {exec} = require('child_process')
 
 app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.use('/css', express.static('css'))
+
+
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + '/html/home.html')
+})
+
+
+
+
+app.listen(8200)
